85d1552c730f2f709179a9222c770669df5d4d69
MohanManirul <fiforeg@gmail.com>
2023-07-04 22:58:18 +0600

    shopping cart

---
+++ cart.js
@@ -0,0 +1,7 @@
+
+const divElement = document.createElement("div");
+divElement.style.fontSize = "12px";
+
+const container = document.querySelector(".cart-item-parent-box");
+
+container.appendChild(divElement);
--- product.js
+++ product.js
@@ -1,8 +1,8 @@
 const myProduct = [
     {
         id : 1,
-        name : "Computer",
-        price : 300,
+        name : "Camera",
+        price : 500,
         img : "https://www.freepik.com/premium-psd/summer-travel-vacation-concept-with-beach-chair-umbrella-summer-vacation-concept_38524488.htm#&position=0&from_view=collections"
 
     },
